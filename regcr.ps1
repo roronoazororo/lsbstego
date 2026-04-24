9fa764707a988e2b9fd036028c0ae6e2278199be
roronoazororo <roronoazororo123@gmail.com>
2026-04-24 18:21:47 +0530

    Deleting unnecessary commits

--- regcr.ps1
+++
@@ -1,9 +0,0 @@
-# Create key
-New-Item -Path "HKCU:\Software\MyTestKey" -Force
-
-# Add a string value
-New-ItemProperty -Path "HKCU:\Software\MyTestKey" `
-  -Name "TestValue" `
-  -Value "pti wrote this" `
-  -PropertyType String `
-  -Force
